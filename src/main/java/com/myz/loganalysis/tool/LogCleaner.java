2e7a9eacc92a5bb5c133f146c83a431a378c6669
frozen007 <zmy1019@163.com>
2013-05-06 18:34:44 +0800

    add

---
+++ src/main/java/com/myz/loganalysis/tool/LogCleaner.java
@@ -0,0 +1,31 @@
+package com.myz.loganalysis.tool;
+
+import java.util.Set;
+
+import com.mongodb.DB;
+import com.myz.loganalysis.MongoDBManager;
+
+public class LogCleaner {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String upDate = "20121231";
+        DB logdb = MongoDBManager.getInstance().getLogDB();
+        Set<String> colNames = logdb.getCollectionNames();
+        for(String colName : colNames) {
+            if(colName.startsWith("log2012")) {
+                String strLogDate = colName.substring(3, colName.indexOf('.'));
+                if(strLogDate.compareTo(upDate) <=0) {
+                    System.out.println(colName + " dropped");
+                    logdb.getCollection(colName).dropIndexes();
+                    logdb.getCollection(colName).drop();
+                }
+            }
+            
+        }
+
+    }
+
+}
