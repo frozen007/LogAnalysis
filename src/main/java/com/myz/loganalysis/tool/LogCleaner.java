1064119f562036906c3b353d0834f3efc1a3f40e
frozen007 <zmy1019@163.com>
2013-05-08 13:52:28 +0800

    add LogCleaner

--- src/main/java/com/myz/loganalysis/tool/LogCleaner.java
+++ src/main/java/com/myz/loganalysis/tool/LogCleaner.java
@@ -11,16 +11,18 @@ public class LogCleaner {
      * @param args
      */
     public static void main(String[] args) {
-        String upDate = "20121231";
+        String upDate = "20130331";
         DB logdb = MongoDBManager.getInstance().getLogDB();
         Set<String> colNames = logdb.getCollectionNames();
         for(String colName : colNames) {
-            if(colName.startsWith("log2012")) {
+            if(colName.startsWith("log2013")) {
                 String strLogDate = colName.substring(3, colName.indexOf('.'));
                 if(strLogDate.compareTo(upDate) <=0) {
                     System.out.println(colName + " dropped");
+                    /*
                     logdb.getCollection(colName).dropIndexes();
                     logdb.getCollection(colName).drop();
+                    */
                 }
             }
             
